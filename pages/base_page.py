761cea988e590d047e2058dd3621674a59e86013
Leonov Eduard <ithmrf1995@mail.ru>
2025-11-01 17:03:00 +0700

    fix: all code review comments have been addressed

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 15)
+        self.wait = WebDriverWait(driver, 10)
     
     def find_element(self, locator):
         return self.wait.until(EC.presence_of_element_located(locator))
